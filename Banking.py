8830081aa215456715d4a2efded45dfb4212ebf5
Sean Davis <sean.dev.davis@gmail.com>
2024-11-15 20:50:17 -0600

    working properly

--- Banking.py
+++ Banking.py
@@ -1,9 +1,6 @@
 import os, sys, time, logging
 from datetime import datetime
-
-##############
-# To do list #
-##############
+import re
 
 # Ask user to insert card
 # Ask for PIN
@@ -11,17 +8,16 @@ from datetime import datetime
 # Ask if they want another transaction
 # Repeat if yes, exit if no
 
-# user_savings = float(20000.00)
-# user_balance = float(3500.00)
-# current_balance = None
-# x = 0
-
+#  starting screen
 print(datetime.now())
 time.sleep(0.7)
 print("Please insert your debit card...")
-time.sleep(3)
+time.sleep(1.4)
 print("Verifying card...")
 time.sleep(2)
+print("Verified!")
+
+approved = True
 
 
 def home_display():
@@ -33,14 +29,13 @@ def home_display():
     print("1. Check balance")
     print("2. Deposit")
     print("3. Withdraw")
-    print("4. Exit")
 
     selection = int(input("Enter 1-4: \n"))
 
     match selection:
         case 1:
             #  check balance
-            time.sleep(1)
+            time.sleep(0.3)
             print(f"Your balance is {balance}")
             while logged_in:
                 selection = input("Would you like another transaction? (yes/no) : ").lower()
@@ -48,25 +43,27 @@ def home_display():
                     time.sleep(0.8)
                     home_display()
                 elif selection == "no":
-                    break
+                    sys.exit()
+                    logged_in = False
                 else:
                     print("Enter yes or no")
         case 2:
             #  deposit
-            time.sleep(1)
-            deposit = input("Please enter deposit amount: ")
-            print(f"You deposited {deposit}")
             while logged_in:
+                time.sleep(0.8)
+                deposit = input("Please enter deposit amount: ")
+                print(f"You deposited {deposit}")
                 selection = input("Would you like another transaction? (yes/no) : ").lower()
                 if selection == "yes":
                     time.sleep(0.8)
                     home_display()
                 elif selection == "no":
-                    break
+                    sys.exit()
+                    logged_in = False
                 else:
                     print("Enter yes or no")
         case 3:
-            time.sleep(1)
+            time.sleep(0.8)
             withdraw = input("Please enter withdrawal amount: ")
             print(f"You withdrew {withdraw}")
             while logged_in:
@@ -75,30 +72,34 @@ def home_display():
                     time.sleep(0.8)
                     home_display()
                 elif selection == "no":
-                    break
+                    sys.exit()
+                    logged_in = False
                 else:
                     print("Enter yes or no")
 
 
 def get_pin():
-    approved = True
     nums = [1234, 5678, 5432, 4321, 1111, 1357, 2468]
 
-    user_pin = int(input("Please enter your PIN: "))
-    print("Checking PIN...")
-    time.sleep(1)
-    while approved:
-        try:
-            if user_pin in nums:
-                print(f"Hello, welcome to your account")
-                home_display()
-            else:
-                print("Incorrect PIN")
-        except Exception as e:
-            print('Numbers only for PIN')
+    try:
+        user_pin = input("Please enter your PIN: ")
+        # print(len(user_pin))
+        if len(user_pin) > 4:
+            print('Invalid input. Please try again')
+    except ValueError:
+        if user_pin.isalpha():
+            print('Invalid input. Numbers only')
+    else:
+        print("Checking PIN...")
+        time.sleep(0.3)
+
+        if int(user_pin) in nums:
+            print(f"Hello, welcome to your account")
+            home_display()
 
 
 get_pin()
+
 
 
 
